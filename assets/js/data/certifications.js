470957b6eee3e4407c687a52f2713dacd5953970
Le Hoang <lehoangnguyen510@gmail.com>
2025-03-18 16:56:11 +0100

    update

--- assets/js/data/certifications.js
+++ assets/js/data/certifications.js
@@ -2,8 +2,36 @@ const certifications = [
     {
         title: "Ingénieur diplômé de l’Institut National des Sciences Appliquées de Rennes",
         desc: "Spécialité Ingénieur Informatique",
-        issue: "INSA Rennes | Issued Sep 2024",
+        issue: "INSA Rennes | Issued Sept 2024",
         img: "assets/img/certifications/insa-rennes.jpg",
         link: "https://certificate.bcdiploma.com/check/4BD93086C90E5B278C1E766E58925CFEB47A99558C1C5160813235C001F6F687TTBpU3F3dDNpNnB2eDU2QkQzT3JvSTRlbGg5a2F2WG4zVFNXOUk5ekVMWno4UGpu"
-    }
+    },
+    {
+        title: "Rest API (Intermediate) Certificate",
+        desc: "Rest API (Intermediate) Certificate",
+        issue: "HackerRank | Issued Jan 2023",
+        img: "assets/img/certifications/HackerRank_Icon.png",
+        link: "https://www.hackerrank.com/certificates/511a457fa8e1"
+    },
+    {
+        title: "Problem Solving (Intermediate) Certificate",
+        desc: "Problem Solving (Intermediate) Certificate",
+        issue: "HackerRank | Issued Jan 2023",
+        img: "assets/img/certifications/HackerRank_Icon.png",
+        link: "https://www.hackerrank.com/certificates/0dffef75e605"
+    },
+    {
+        title: "Angular (Intermediate) Certificate",
+        desc: "Angular (Intermediate) Certificate",
+        issue: "HackerRank | Issued Dec 2022",
+        img: "assets/img/certifications/HackerRank_Icon.png",
+        link: "https://www.hackerrank.com/certificates/20e6dcb5813d"
+    },
+    {
+        title: "SQL (Advanced) Certificate",
+        desc: "SQL (Advanced) Certificate",
+        issue: "HackerRank | Issued Dec 2022",
+        img: "assets/img/certifications/HackerRank_Icon.png",
+        link: "https://www.hackerrank.com/certificates/506ede556dfc"
+    },
 ]
